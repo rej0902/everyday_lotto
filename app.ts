65998aa89663c9b8b8cba099b14d29ffb2e0b812
riky <riky@myriky.net>
2023-05-19 11:33:55 +0900

    initial commit

---
+++ app.ts
@@ -0,0 +1,125 @@
+import puppeteer from 'puppeteer';
+import * as dotenv from 'dotenv';
+import { sendImageToSlack, sendMessageToSlack } from './slack';
+dotenv.config();
+
+const URL_LOGIN = 'https://dhlottery.co.kr/user.do?method=login';
+const URL_GAME = 'https://ol.dhlottery.co.kr/olotto/game/game645.do';
+
+const SELECTOR_ID_FOR_LOGIN = '[name="userId"]';
+const SELECTOR_PASSWORD_FOR_LOGIN = '[name="password"]';
+
+const SELECTOR_BUTTON_FOR_WAY_TO_BUY = 'ul#tabWay2Buy > li:nth-child(2) > a';
+const SELECTOR_SELECT_FOR_AMOUNT = 'select#amoundApply';
+const SELECTOR_BUTTON_FOR_AMOUNT = 'input#btnSelectNum';
+const SELECTOR_BUTTON_FOR_BUY = 'input#btnBuy';
+const SELECTOR_BUTTON_FOR_CONFIRM = 'input[value="확인"]';
+
+const ENV_USER_ID = process.env.DH_LOTTERY_USER_ID;
+const ENV_USER_PW = process.env.DH_LOTTERY_PASSWORD;
+
+const ENV_AMOUNT = process.env.AMOUNT_PER_DAY || '1';
+
+const lotto = async () => {
+  console.log('=== 오 늘 의 로 또 ===');
+  if (ENV_USER_ID === undefined || ENV_USER_PW === undefined) {
+    throw new Error(`DH_LOTTERY_USER_ID, DH_LOTTERY_PASSWORD must be defined in .env file`);
+  }
+
+  const USER_ID = ENV_USER_ID;
+  const USER_PW = ENV_USER_PW;
+  const AMOUNT = ENV_AMOUNT;
+
+  console.log(`USER_ID => ${USER_ID}`);
+  console.log(`USER_PASSWORD => ${USER_PW.replace(/./g, '*')}`);
+  console.log(`envionment loaded!`);
+
+  const browser = await puppeteer.launch({ headless: 'new' });
+  const page = await browser.newPage();
+
+  console.log('[1] navigate to DH LOTTERY login page...');
+
+  await page.goto(URL_LOGIN);
+
+  await page.setViewport({ width: 1080, height: 1024 });
+
+  await page.waitForSelector(SELECTOR_ID_FOR_LOGIN);
+
+  console.log('[2] prepare login...');
+
+  await page.type(SELECTOR_ID_FOR_LOGIN, USER_ID);
+  await page.type(SELECTOR_PASSWORD_FOR_LOGIN, USER_PW);
+  await page.keyboard.press('Enter');
+
+  console.log('[3] try login...');
+
+  await page.waitForNavigation();
+
+  console.log('[4] login completed!');
+
+  await page.goto(URL_GAME);
+
+  console.log('[5] waiting for buy a game...');
+
+  await page.waitForSelector(SELECTOR_BUTTON_FOR_BUY);
+
+  await page.click(SELECTOR_BUTTON_FOR_WAY_TO_BUY);
+
+  console.log(`[6] 사장님 자동 ${AMOUNT}게임요~~`);
+
+  await page.select(SELECTOR_SELECT_FOR_AMOUNT, AMOUNT);
+
+  await page.click(SELECTOR_BUTTON_FOR_AMOUNT);
+
+  await page.click(SELECTOR_BUTTON_FOR_BUY);
+
+  console.log('[7] waiting for confirm...');
+
+  const confirm_buttons = await page.$$(SELECTOR_BUTTON_FOR_CONFIRM);
+
+  const confirm = confirm_buttons?.pop();
+
+  if (confirm) {
+    console.log('[8] confirming...');
+    await confirm.evaluate((e) => e.click());
+  }
+
+  try {
+    await page.waitForSelector('div#report', {
+      visible: true,
+      timeout: 1000,
+    });
+
+    await page.evaluate(() => {
+      console.log('[9] remove unnecessary elements...');
+      document.querySelector('div.n720PlusBanner')?.remove();
+      document.querySelector('#popReceipt h2')?.remove();
+      document.querySelector('input#closeLayer')?.remove();
+      document.querySelector('div.explain')?.remove();
+    });
+
+    const result = await page.$('#popReceipt');
+
+    console.log('[10] screenshot...');
+
+    const b64string: string = (await result?.screenshot({ encoding: 'base64' })) as string;
+
+    //슬랙을 사용하려면 해당 주석을 풀고, .env 파일에 SLACK_BOT_TOKEN을 추가해야 합니다.
+
+    sendImageToSlack({
+      base64fromImage: b64string,
+    });
+
+    console.log('[11] job completed!');
+  } catch (error) {
+    //슬랙을 사용하려면 해당 주석을 풀고, .env 파일에 SLACK_BOT_TOKEN을 추가해야 합니다.
+
+    sendMessageToSlack({
+      message: '이번주 로또 구매는 실패했습니다.....',
+    });
+    console.error('[-] job failed!');
+  }
+
+  await browser.close();
+};
+lotto();
---
+++ slack.ts
@@ -0,0 +1,89 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import request from 'request';
+
+const SLACK_UPLOAD_API_URL = 'https://slack.com/api/files.upload';
+const SLACK_WRITE_API_URL = 'https://slack.com/api/chat.postMessage';
+
+const ENV_SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN;
+const ENV_CHANNEL = process.env.SLACK_CHANNEL_ID;
+
+const sendImageToSlack = ({
+  base64fromImage,
+
+  filename = 'lotto.png',
+  filetype = 'image/png',
+  message = '설레는 토요일! 오늘의 로또가 발급됐읍니다. (https://dhlottery.co.kr/myPage.do?method=lottoBuyListView)',
+  title = '오늘의 로또',
+}: {
+  base64fromImage: string;
+  filename?: string;
+  filetype?: string;
+  title?: string;
+  message?: string;
+}) => {
+  if (ENV_SLACK_BOT_TOKEN === undefined || ENV_CHANNEL === undefined) {
+    throw new Error(`SLACK_BOT_TOKEN, SLACK_CHANNEL must be defined in .env file`);
+  }
+
+  console.log(`SLACK_BOT_TOKEN => ${ENV_SLACK_BOT_TOKEN}`);
+  console.log(`SLACK_CHANNEL => ${ENV_CHANNEL}`);
+
+  const buffer: Buffer = Buffer.from(base64fromImage, 'base64');
+
+  request.post(
+    {
+      url: SLACK_UPLOAD_API_URL,
+      formData: {
+        channels: ENV_CHANNEL,
+        file: {
+          value: buffer,
+          options: {
+            filename,
+          },
+        },
+        filename,
+        filetype,
+        token: ENV_SLACK_BOT_TOKEN,
+        initial_comment: message,
+        title,
+      },
+    },
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(`[${ENV_CHANNEL}] Send image to slack completed!`);
+      } else {
+        console.error(error);
+      }
+    }
+  );
+};
+
+const sendMessageToSlack = ({ message }: { message: string }) => {
+  if (ENV_SLACK_BOT_TOKEN === undefined || ENV_CHANNEL === undefined) {
+    throw new Error(`SLACK_BOT_TOKEN, SLACK_CHANNEL must be defined in .env file`);
+  }
+
+  console.log(`SLACK_BOT_TOKEN => ${ENV_SLACK_BOT_TOKEN}`);
+  console.log(`SLACK_CHANNEL => ${ENV_CHANNEL}`);
+
+  request.post(
+    {
+      url: SLACK_WRITE_API_URL,
+      formData: {
+        channel: ENV_CHANNEL,
+        text: message,
+        token: ENV_SLACK_BOT_TOKEN,
+      },
+    },
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(`[${ENV_CHANNEL}] Send message to slack completed!`);
+      } else {
+        console.error(error);
+      }
+    }
+  );
+};
+
+export { sendImageToSlack, sendMessageToSlack };
